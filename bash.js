267e442639eef0e59aad8f43661a2cc96e976cc4
j-dags <jon.dagdagan@gmail.com>
2020-11-13 14:40:43 -0600

    Curl implemented

--- bash.js
+++ bash.js
@@ -1,6 +1,7 @@
 const pwd = require("./pwd");
 const ls = require("./ls");
 const cat = require("./cat");
+const curl = require("./curl");
 
 process.stdout.write("prompt > ");
 
@@ -8,11 +9,15 @@ process.stdin.on("data", (data, fnc) => {
   const prompt = data.toString().trim().split(" ");
 
   process.stdout.write("\nYou typed: " + prompt.join(" "));
+  console.log("\n");
+
   if (prompt[0] === "pwd") {
     pwd();
   } else if (prompt[0] === "ls") {
     ls();
   } else if (prompt[0] === "cat") {
     cat(prompt[1]);
+  } else if (prompt[0] === "curl") {
+    curl(prompt[1]);
   }
 });
