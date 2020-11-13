8a3cdbb4beaf4a7173a40e5a1cfb23e9526bbaa4
Alfred Meng <alfredfmeng@gmail.com>
2020-11-13 12:07:22 -0600

    started modules section

--- bash.js
+++ bash.js
@@ -1,8 +1,14 @@
+const pwd = require('./pwd')
+const ls = require('./ls')
+// process.stdout.write("prompt > ");
+
+// process.stdin.on("data", (data) => {
+//   const cmd = data.toString().trim();
+
+//   if (cmd === "pwd") process.stdout.write(process.cwd());
+//   process.stdout.write('\nYou typed: ' + cmd);
+//   process.stdout.write("\nprompt > ");
+// });
+
 process.stdout.write("prompt > ");
-
-process.stdin.on("data", (data) => {
-  const cmd = data.toString();
-
-  if (cmd === "pwd") process.stdout.write(process.cwd());
-  process.stdout.write("\nprompt > ");
-});
+pwd();
